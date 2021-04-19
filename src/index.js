fa484113bbc6e67a6500ab1baf0e03b3214fba9d
pawelmilewczyk <milewczykp@gmail.com>
2021-04-19 15:55:12 +0200

    Create redux store

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import reducer from "./store/reducer";
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
