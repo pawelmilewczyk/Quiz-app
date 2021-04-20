adb2c2cfe33e13cefe12503e39b8c94478b40711
pawelmilewczyk <milewczykp@gmail.com>
2021-04-20 10:13:57 +0200

    Update style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
 import classes from "./App.module.scss";
-// import axios from "axios";
 import Menu from "./containers/Menu/Menu";
 
 function App() {
-  // const instance = axios.create({
-  //   baseURL: "https://quizapi.io/api/v1/questions",
-  //   params: {
-  //     apiKey: "tygm5wOzRUEC8rkoLMWhPh6GvKr6UA9KX3GOCqvn",
-  //     // category: linux, SQL, DevOps, Docker, Code, CMS,
-  //     tags: "HTML",
-  //     //tags: JavaScript, DevOps, HTML, Linux, Docker, PHP
-  //     limit: 10,
-  //     difficulty: "easy",
-  //   },
-  // });
-  // instance
-  //   .get()
-  //   .then((res) => console.log(res.data))
-  //   .catch((err) => console.log(err));
-
   return (
     <div className={classes.App}>
       <Menu />
