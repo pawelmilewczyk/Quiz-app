343a45fc4bd9159df88bdd660b393a70556693eb
pawelmilewczyk <milewczykp@gmail.com>
2021-04-20 10:22:42 +0200

    Create Quiz container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import classes from "./App.module.scss";
 import Menu from "./containers/Menu/Menu";
+import Quiz from "./containers/Quiz/Quiz";
 
 function App() {
   return (
     <div className={classes.App}>
       <Menu />
+      <Quiz />
     </div>
   );
 }
---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,11 @@
+import Title from "../../components/Title/Title";
+
+const Quiz = (props) => {
+  return (
+    <div className="Container">
+      <Title>Course name</Title>
+    </div>
+  );
+};
+
+export default Quiz;
