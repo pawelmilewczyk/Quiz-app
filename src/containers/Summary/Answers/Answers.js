5d7e3b2bd21546c296a722ea2a8195a2c1adaef9
pawelmilewczyk <milewczykp@gmail.com>
2021-04-22 16:28:31 +0200

    Add answers from state

--- src/containers/Summary/Answers/Answers.js
+++ src/containers/Summary/Answers/Answers.js
@@ -6,54 +6,53 @@ import { FormControlLabel, Checkbox } from "@material-ui/core";
 import "./Answers.scss";
 
 const Answers = (props) => {
+  const renderAnswer = (index, answers) => {
+    let indexes = [];
+    answers[index].map((el, indexOfTrue) => {
+      if (el === "true") indexes.push(indexOfTrue);
+    });
+
+    return Object.values(props.data[index]?.answers).map((answer, i) => {
+      if (answer) {
+        let checkbox;
+        if (indexes.includes(i))
+          checkbox = <Checkbox checked={true} disabled={true} />;
+        else checkbox = <Checkbox checked={false} disabled={true} />;
+        return (
+          <FormControlLabel
+            key={Math.random()}
+            control={checkbox}
+            label={answer}
+          />
+        );
+      }
+    });
+  };
+
+  const content = props.data.map((el, index) => (
+    <div className="Question_Container" key={index}>
+      <div className="Question">
+        Question <span>{index + 1}</span>
+      </div>
+      <div className="Question_Content Question">
+        Question: <span>{el.question}</span>
+      </div>
+      <div className="Answers Answers_Given">
+        <div className="Answers_Title">Your Answers</div>
+        {renderAnswer(index, props.givenAnswers)}
+      </div>
+      <div className="Answers Answers_Correct">
+        <div className="Answers_Title">Correct Answers</div>
+        {renderAnswer(index, props.correctAnswers)}
+      </div>
+      <div className="feedback">GOOD</div>
+    </div>
+  ));
+
   return (
     <div>
       <Horizontal>Answers</Horizontal>
-      <div className="Question_Container">
-        <div className="Question">
-          Question <span>1</span>
-        </div>
-        <div className="Question_Content Question">
-          Question: <span>{props.data[0]?.question}</span>
-        </div>
-        <div className="Answers Answers_Given">
-          <div className="Answers_Title">Your Answers</div>
-          <FormControlLabel
-            key="key"
-            control={
-              <Checkbox checked={true} color="default" disabled={true} />
-            }
-            label="Answer A"
-          />
-          <FormControlLabel
-            className="correct"
-            key="key"
-            control={
-              <Checkbox checked={true} color="default" disabled={true} />
-            }
-            label="Answer B"
-          />
-        </div>
-        <div className="Answers Answers_Correct">
-          <div className="Answers_Title">Correct Answers</div>
-          <FormControlLabel
-            className="wrong"
-            key="key"
-            control={
-              <Checkbox checked={false} color="default" disabled={true} />
-            }
-            label="Answer A"
-          />
-          <FormControlLabel
-            key="key"
-            control={
-              <Checkbox checked={true} color="default" disabled={true} />
-            }
-            label="Answer B"
-          />
-        </div>
-        <div className="feedback">GOOD</div>
-      </div>
+      {props.data.length ? content : null}
     </div>
   );
 };
@@ -61,10 +60,8 @@ const Answers = (props) => {
 const mapStateToProps = (state) => {
   return {
     data: state.data,
-    // tag: state.data[0]?.tags,
-    // score: state.score,
     correctAnswers: state.correctAnswers,
-    givenAnswers: state.givenAnswers,
+    givenAnswers: state.givenAnswers.map((el) => el.map((el) => el.toString())),
   };
 };
 
