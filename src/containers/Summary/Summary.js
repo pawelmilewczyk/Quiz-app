f72a6ec6de02ce1faa9b1f5ea29af5b78f03cc07
pawelmilewczyk <milewczykp@gmail.com>
2021-04-24 09:14:12 +0200

    Set delay of rendering summary container

--- src/containers/Summary/Summary.js
+++ src/containers/Summary/Summary.js
@@ -1,14 +1,28 @@
 import Score from "./Score/Score";
 import Answers from "./Answers/Answers";
 import Buttons from "./Buttons/Buttons";
+import Spinner from "../../components/Spinner/Spinner";
+import Aux from "../../components/Aux/Aux";
+
+import { useEffect, useState } from "react";
 
 const Summary = () => {
-  return (
-    <div>
+  const [summary, setSummary] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSummary(true);
+    }, 500);
+  });
+
+  return summary ? (
+    <Aux>
       <Score />
       <Answers />
       <Buttons />
-    </div>
+    </Aux>
+  ) : (
+    <Spinner />
   );
 };
 
